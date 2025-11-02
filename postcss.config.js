e367e652120778e1e0f48ec68787cf15ee644710
pg359201-sudo <pg359201@gmail.com>
2025-11-02 15:47:22 -0300

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+4. **"Commit changes"**
+
+---
+
+#### 📄 Archivo 6: `.gitignore`
+
+1. **"Add file"** → **"Create new file"**
+2. Nombre: `.gitignore`
+3. Contenido:
+```
+node_modules
+dist
+.DS_Store
